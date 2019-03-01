980faceadcdb3668fd3f128738c4dc5e1da8c4c6
dipto0321 <diptokmk47@gmail.com>
2019-03-01 09:00:42 +0600

    Refactor fetchCity function
    - Use async await to complete this job

--- src/fetchCity.js
+++ src/fetchCity.js
@@ -1,5 +1,9 @@
-const fetchCity = (city) => {
-  console.log(city);
-};
+async function fetchCity(city) {
+  const yacdn = `https://yacdn.org/proxy/`
+  const url = `${yacdn}https://www.metaweather.com/api/location/search/?query=${city.toLowerCase()}`;
+
+  const data = await fetch(url).then(data => data.json());
+  return data[0].woeid;
+}
 
 export default fetchCity;
--- src/fetchCityData.js
+++ src/fetchCityData.js
@@ -1,21 +1,8 @@
 import fetchCity from './fetchCity';
 
-const fetchCityData = (city) => {
-  // Based on input city -> toLowerCase first
-  let woeid;
-  console.log(city.toLowerCase());
-  const url = `https://www.metaweather.com/api/location/search/?query=${city.toLowerCase()}`;
-  fetch(url, {
-      mode: "cors",
-    })
-    .then((data) => {
-      return data.json();
-    })
-    .then(data => {
-      woeid = data[0].woeid;
-      // console.log(data[0].woeid)
-    });
-  console.log("WOEID:", woeid);
-};
+async function fetchCityData(city) {
+  const woeid = fetchCity(city);
+  console.log(woeid);
 
+}
 export default fetchCityData;
