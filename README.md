77483a779a58c065492ad5a5b7cbb07c9e3da00b
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-03-01 09:54:33 +0800

    Initial commit for the project and webpack setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# JavaScript Weather App
+
+## by [Ryan](https://github.com/rvvergara) and [Dipto](https://github.com/dipto0321)
+
+### [Live Demo](#)
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [{
+        test: /\.txt$/,
+        loader: "raw-loader",
+      },
+      {
+        test: /\.css$/,
+        loader: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+};
