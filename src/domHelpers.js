ed753ba5801bae64874ef42997e10e21c4be5bcc
dipto0321 <diptokmk47@gmail.com>
2019-03-04 13:37:34 +0600

    Create call back function for radio button
    - Extract get Card Elements logic  into a function for re-using

--- src/domHelpers.js
+++ src/domHelpers.js
@@ -3,24 +3,15 @@ import {
 } from "path";
 
 let tempUnitC = true;
-
+let fetchedWeatherData;
 const toggleTempUnit = () => {
   tempUnitC = !tempUnitC;
-  console.log(tempUnitC);
 }
 
-[...document.getElementsByName("temp")].forEach(radio => {
-  radio.addEventListener("change", e => {
-    e.stopPropagation();
-    // toggleTempUnit();
-  });
-});
+// Function to generate card elements
 
-const showData = (dataArr) => {
-  // Generate dynamic HTML for big card
-  // Assign dataObj elements in DOM
-  document.getElementById("cityName").innerText = dataArr[0];
-  const cardElements = {
+const getCardElements = () => {
+  return {
     foreCastDates: [...document.getElementsByClassName("forecast-date")],
     weatherStateImgs: [...document.getElementsByClassName("weather-state")],
     theTemps: [...document.getElementsByClassName("the-temp")],
@@ -29,6 +20,23 @@ const showData = (dataArr) => {
     humidityDisplays: [...document.getElementsByClassName("humidity")],
     unitSpans: [...document.getElementsByClassName("temp-unit")],
   };
+};
+
+[...document.getElementsByName("temp")].forEach(radio => {
+  radio.addEventListener("change", e => {
+    e.stopPropagation();
+    toggleTempUnit();
+    fetchedWeatherData.forEach((data, index) => tempDisplays(data, getCardElements(), index));
+  });
+});
+
+
+const showData = (dataArr) => {
+  // Generate dynamic HTML for big card
+  // Assign dataObj elements in DOM
+  fetchedWeatherData = dataArr[1];
+  document.getElementById("cityName").innerText = dataArr[0];
+  const cardElements = getCardElements();
   dataArr[1].forEach((data, index) => {
     weatherCard(data, index, cardElements);
   });
@@ -47,10 +55,10 @@ const weatherCard = (data, index, cardElements) => {
     humidity,
     applicable_date,
   } = data;
-  // console.log(data);
   cardElements.foreCastDates[index].innerText = parseDate(applicable_date);
   cardElements.weatherStateImgs[index].setAttribute("src", `https://www.metaweather.com/static/img/weather/png/${weather_state_abbr}.png`);
   // Temperature displays
+
   tempDisplays({
     the_temp,
     min_temp,
